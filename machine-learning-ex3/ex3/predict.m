2e6345d6378db66fb65d1ea0268d088ce24a1f03
alan.lee <alanlee1985@gmail.com>
2016-12-21 14:50:12 +0800

    ex3 finished

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -7,12 +7,12 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
@@ -21,12 +21,15 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+X = [ones(m, 1) X];
 
+p_matix1 = sigmoid(X * Theta1'); %  p_matix1 should be m * 25
 
+p_matix1 = [ones(m, 1) p_matix1];
 
+p_matix2 = sigmoid(p_matix1 * Theta2');
 
-
-
+[val, p] = max(p_matix2, [], 2);
 
 
 % =========================================================================
